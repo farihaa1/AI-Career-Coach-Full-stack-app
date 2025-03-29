bcd2acd0604cbeccaf50e66908168639d6a63715
farihaa1 <farihaa1403@gmail.com>
2025-03-29 12:11:33 +0600

    created landing page

--- app/layout.js
+++ app/layout.js
@@ -33,7 +33,7 @@ export default function RootLayout({ children }) {
             <header>
               <Header></Header>
             </header>
-            <main> {children}</main>
+            <main className=""> {children}</main>
             <footer className="bg-muted/50 py-12">
               <div className="container mx-auto px-4 text-center text-gray-200">
                 <p>Made by Johra Khatun fariha</p>
---
+++ components/HeroSection.jsx
@@ -0,0 +1,61 @@
+"use client";
+import Link from "next/link";
+import React, { useEffect, useRef } from "react";
+import { Button } from "./ui/button";
+import Image from "next/image";
+
+const HeroSection = () => {
+  const imageRef = useRef(null);
+  useEffect(() => {
+    const imageElement = imageRef.current;
+
+    const handleScroll = () => {
+      const scrollPosition = window.scrollY;
+      const scrollThreshold = 100;
+      if (scrollPosition > scrollThreshold) {
+        imageElement.classList.add("scrolled");
+      }
+      else {
+        imageElement.classList.remove("scrolled")
+      }
+    };
+    window.addEventListener("scroll",handleScroll)
+    return ()=> window.removeEventListener("scroll",handleScroll)
+  }, []);
+
+  return (
+    <section className="w-full pt-36 md:pt-48 pb-10 space-y-6 text-center">
+      <div className="space-y-6 mx-auto">
+        <h1 className="gradient-title text-5xl font-bold md:text-6xl lg:text-7xl xl:text-8xl gradient">
+          Your AI Career Coach For <br />
+          Professional Success
+        </h1>
+        <p className="mx-auto max-w-[600px] text-muted-foreground md:text-xl">
+          Advanced career guides with personalized guidance, interview prep, and
+          AI-powered tools for job success.
+        </p>
+      </div>
+      <div className="flex space-x-4 justify-center">
+        <Link href="/dashboard">
+          <Button size="lg" className="px-8">
+            Get Started
+          </Button>
+        </Link>
+      </div>
+      <div className="hero-image-wrapper mt-5 md:mt-0">
+        <div ref={imageRef} className="hero-image">
+          <Image
+            src={"/banner.jpeg"}
+            width={1280}
+            height={720}
+            alt="banner ai career coach"
+            className="rounded-lg shadow-2xl border mx-auto"
+            priority
+          ></Image>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default HeroSection;
